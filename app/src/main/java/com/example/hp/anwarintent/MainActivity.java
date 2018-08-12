0ed058338a2a1c158e324509ce71bdc4e25e4c97
HP <droid.anwarsaputra@gmail.com>
2018-08-12 09:30:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hp/anwarintent/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hp.anwarintent;
+
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void panggil(View view) {
+        String nomor = "087884692053" ;
+        Intent panggil = new Intent(Intent. ACTION_DIAL);
+        panggil.setData(Uri. fromParts("tel",nomor,null));
+        startActivity(panggil);
+    }
+
+    public void buka(View view) {
+        String url = "https://onanwar.com" ;
+        Intent bukabrowser = new Intent(Intent. ACTION_VIEW);
+        bukabrowser.setData(Uri. parse(url));
+        startActivity(bukabrowser);
+    }
+    public void tentang(View view) {
+        Intent tentang= new Intent(MainActivity.this, Tentang.class);
+        startActivity(tentang);
+    }
+}
